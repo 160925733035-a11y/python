b14f7cae2fc4b8b390cb73d4fbaa49bfee552654
160925733035-a11y <160925733035@lords.ac.in>
2026-09-08 11:05:12 +0530

    Create greater.py

---
+++ greater.py
@@ -0,0 +1,12 @@
+# Find greater of two numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("First number is greater")
+elif b > a:
+    print("Second number is greater")
+else:
+    print("Both numbers are equal")
+
