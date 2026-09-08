9596eb6834e98707fd8814c3742724a60b04f421
160925733035-a11y <160925733035@lords.ac.in>
2026-09-08 12:17:49 +0530

    Create three.py

---
+++ three.py
@@ -0,0 +1,12 @@
+```
+# Check if three numbers are equal
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a == b and b == c:
+    print("All three numbers are equal")
+else:
+    print("All three numbers are not equal")
+```
